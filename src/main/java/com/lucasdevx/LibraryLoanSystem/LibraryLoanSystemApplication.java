c45a7f3ebd0e4d66f18ecb177efa6b82ae24030c
Lucas Lima <lucaslimadevx@gmail.com>
2025-11-23 14:28:31 -0300

    Configurated database url,drive, user and passoword

---
+++ src/main/java/com/lucasdevx/LibraryLoanSystem/LibraryLoanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdevx.LibraryLoanSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryLoanSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryLoanSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasdevx/LibraryLoanSystem/LibraryLoanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdevx.LibraryLoanSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryLoanSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
