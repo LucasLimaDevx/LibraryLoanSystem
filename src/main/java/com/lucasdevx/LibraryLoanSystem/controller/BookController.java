c9db53e766356645d89927de4c41b4abae718357
Lucas Lima <lucaslimadevx@gmail.com>
2025-11-26 00:25:43 -0300

    Implements insert, getById, getAll, update and delete methods for Book entity

--- src/main/java/com/lucasdevx/LibraryLoanSystem/controller/BookController.java
+++ src/main/java/com/lucasdevx/LibraryLoanSystem/controller/BookController.java
@@ -1,10 +1,64 @@
 package com.lucasdevx.LibraryLoanSystem.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasdevx.LibraryLoanSystem.dto.BookDTO;
+import com.lucasdevx.LibraryLoanSystem.model.Book;
+import com.lucasdevx.LibraryLoanSystem.service.BookService;
 
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
-
+	@Autowired
+	private BookService bookService;
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public BookDTO insert(@RequestBody BookDTO bookDTO) {
+		Book book = bookService.insert( bookService.parseToBook(bookDTO));
+		
+		return bookService.parseToBookDTO(book);
+	}
+	
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public BookDTO getById(@PathVariable Long id) {
+		Book book = bookService.getById(id);
+		return bookService.parseToBookDTO(book);
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<BookDTO> getAll(){
+		List<Book> books = bookService.getAll();
+		List<BookDTO> booksDTO = books.stream()
+				.map((book) -> bookService.parseToBookDTO(book))
+				.collect(Collectors.toList());
+		
+		return booksDTO;
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public BookDTO update(@RequestBody BookDTO bookDTO) {
+		Book newBook = bookService.parseToBook(bookDTO);
+		Book bookUpdated = bookService.update(newBook);
+		
+		return bookService.parseToBookDTO(bookUpdated);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		bookService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/lucasdevx/LibraryLoanSystem/service/BookService.java
+++ src/main/java/com/lucasdevx/LibraryLoanSystem/service/BookService.java
@@ -1,8 +1,65 @@
 package com.lucasdevx.LibraryLoanSystem.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.lucasdevx.LibraryLoanSystem.dto.BookDTO;
+import com.lucasdevx.LibraryLoanSystem.model.Book;
+import com.lucasdevx.LibraryLoanSystem.repository.BookRepository;
 
 @Service
 public class BookService {
-
+	@Autowired
+	private BookRepository bookRepository;
+	
+	public Book insert(Book book) {
+		
+		return bookRepository.save(book);
+	}
+	
+	public Book getById(Long id) {
+		
+		return bookRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Invalid id: " + id));
+	}
+	
+	public List<Book> getAll(){
+		return bookRepository.findAll();
+	}
+	
+	public Book update(Book book) {
+		Book currentBook = getById(book.getId());
+		
+		
+		currentBook.setTitle(book.getTitle());
+		currentBook.setYear(book.getYear());
+		currentBook.setQuantityCopies(book.getQuantityCopies());
+		
+		return bookRepository.save(currentBook);
+		
+	}
+	
+	public void delete(Long id) {
+		bookRepository.deleteById(id);
+	}
+	public Book parseToBook(BookDTO bookDTO) {
+		Book book = new Book();
+		
+		book.setId(bookDTO.id());
+		book.setTitle(bookDTO.title());
+		book.setYear(bookDTO.year());
+		book.setQuantityCopies(bookDTO.quantityCopies());
+		
+		return book;
+	}
+	
+	public BookDTO parseToBookDTO(Book book) {
+		return new BookDTO(
+				book.getId(),
+				book.getTitle(),
+				book.getYear(),
+				book.getQuantityCopies());
+		
+	}
 }
