cee39e243f74df9d750f1e510d3b25bb10cb46da
Lucas Lima <lucaslimadevx@gmail.com>
2025-11-26 00:06:13 -0300

    Implements insert, getById, getAll, update and delete methods for Author entity

--- src/main/java/com/lucasdevx/LibraryLoanSystem/controller/AuthorController.java
+++ src/main/java/com/lucasdevx/LibraryLoanSystem/controller/AuthorController.java
@@ -1,10 +1,64 @@
 package com.lucasdevx.LibraryLoanSystem.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasdevx.LibraryLoanSystem.dto.AuthorDTO;
+import com.lucasdevx.LibraryLoanSystem.model.Author;
+import com.lucasdevx.LibraryLoanSystem.service.AuthorService;
 
 @RestController
 @RequestMapping("/api/author")
 public class AuthorController {
-
+	@Autowired
+	private AuthorService authorService;
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public AuthorDTO insert(@RequestBody AuthorDTO authorDTO) {
+		Author author = authorService.insert( authorService.parseToAuthor(authorDTO));
+		
+		return authorService.parseToAuthorDTO(author);
+	}
+	
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public AuthorDTO getById(@PathVariable Long id) {
+		Author author = authorService.getById(id);
+		return authorService.parseToAuthorDTO(author);
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<AuthorDTO> getAll(){
+		List<Author> authors = authorService.getAll();
+		List<AuthorDTO> authorsDTO = authors.stream()
+				.map((author) -> authorService.parseToAuthorDTO(author))
+				.collect(Collectors.toList());
+		
+		return authorsDTO;
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public AuthorDTO update(@RequestBody AuthorDTO authorDTO) {
+		Author newAuthor = authorService.parseToAuthor(authorDTO);
+		Author authorUpdated = authorService.update(newAuthor);
+		
+		return authorService.parseToAuthorDTO(authorUpdated);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		authorService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/lucasdevx/LibraryLoanSystem/service/AuthorService.java
+++ src/main/java/com/lucasdevx/LibraryLoanSystem/service/AuthorService.java
@@ -1,8 +1,63 @@
 package com.lucasdevx.LibraryLoanSystem.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.lucasdevx.LibraryLoanSystem.dto.AuthorDTO;
+import com.lucasdevx.LibraryLoanSystem.model.Author;
+import com.lucasdevx.LibraryLoanSystem.repository.AuthorRepository;
 
 @Service
 public class AuthorService {
-
+	@Autowired
+	private AuthorRepository authorRepository;
+	
+	public Author insert(Author author) {
+		
+		return authorRepository.save(author);
+	}
+	
+	public Author getById(Long id) {
+		
+		return authorRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Invalid id: " + id));
+	}
+	
+	public List<Author> getAll(){
+		return authorRepository.findAll();
+	}
+	
+	public Author update(Author author) {
+		Author currentAuthor = getById(author.getId());
+		
+		currentAuthor.setName(author.getName());
+		currentAuthor.setNationality(author.getNationality());
+		
+		
+		return authorRepository.save(currentAuthor);
+		
+	}
+	
+	public void delete(Long id) {
+		authorRepository.deleteById(id);
+	}
+	public Author parseToAuthor(AuthorDTO authorDTO) {
+		Author author = new Author();
+		
+		author.setId(authorDTO.id());
+		author.setName(authorDTO.name());
+		author.setNationality(authorDTO.nationality());
+		
+		return author;
+	}
+	
+	public AuthorDTO parseToAuthorDTO(Author author) {
+		return new AuthorDTO(
+				author.getId(),
+				author.getName(),
+				author.getNationality());
+		
+	}
+	
 }
