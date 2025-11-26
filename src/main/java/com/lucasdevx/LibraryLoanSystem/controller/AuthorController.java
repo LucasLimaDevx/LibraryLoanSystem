7fb6149f2b216b7eefaaf30c53f069a3c5a3eeb2
Lucas Lima <lucaslimadevx@gmail.com>
2025-11-25 23:38:36 -0300

    Create AuthorController and AuthorService

---
+++ src/main/java/com/lucasdevx/LibraryLoanSystem/controller/AuthorController.java
@@ -0,0 +1,10 @@
+package com.lucasdevx.LibraryLoanSystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/author")
+public class AuthorController {
+
+}
